801edf3af5df81e2e300b3be9cab04a854c9adb9
gregswift <gregswift@e642da56-8247-0410-bdd1-7d942bd8f7e4>
2010-03-16 02:44:01 +0000

    renamed devl tree to distutils
    git-svn-id: https://keploy.googlecode.com/svn/branches/distutils@20 e642da56-8247-0410-bdd1-7d942bd8f7e4

--- keploy/__init__.py
+++ keploy/__init__.py
@@ -1,18 +1,268 @@
-# This file is part of the keploy SSH Key Management utility.
-#   
-# Copyright (C) 2008 Greg Swift <gregswift@gmail.com>
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# keploy - SSH Public Key Deplyment Utility
+# Copyright © 2007 Greg Swift gregswift@gmail.com
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import signal
+import sys
+
+"""
+ Define Variables
+"""
+DEBUG = True
+VERSION = "0.5"
+
+PW_WARN = '\nNOTICE: You may be prompted for you password,\n'
+PW_WARN += 'NOTICE: this is directly from the ssh client, not keploy\n'
+
+class KeployVariables:
+  def __init__(self, user=None):
+    self.ssh_bin = findSSHbin()
+    self.ssh_config = '/etc/ssh/ssh_config'
+    if (not os.access(self.ssh_config, os.R_OK)):
+      self.ssh_config = None
+    self.ssh_home_dir = os.path.expanduser('~/.ssh')
+    self.tmp_file = os.path.join(self.ssh_home_dir, 'keyploy.tmp')
+    self.id_files = (os.path.join(self.ssh_home_dir, 'id_rsa.pub'),
+                     os.path.join(self.ssh_home_dir,'id_dsa.pub'),
+                     os.path.join(self.ssh_home_dir,'identity.pub'))
+    self.host_files = [os.path.join(self.ssh_home_dir,'known_hosts'),
+                       os.path.join(self.ssh_home_dir,'known_hosts2')]
+    self.auth_keys_file = os.path.join(self.ssh_home_dir,'authorized_keys')
+    self.user = user
+
+  def set(self, name, value):
+    setattr(self, name, value)
+
+"""
+ Define Output Functions and cleanUp Function
+"""
+def standardOut(msg, on=True):
+  """When quiet is not enabled, display msg provided """
+  if (on):
+    print msg
+
+def debugOut(msg, name=None, on=DEBUG):
+  """When debug is enabled, output is printed to the display."""
+  if (name is not None):
+    name = name+': '
+  else:
+    name = ''
+  try:
+    if (on):
+      print 'DEBUG: %s%s' % (name, msg)
+    else:
+      pass
+  except:
+    print 'MALFORMED DEBUG: %s%s' % (name, msg)
+
+def errorOut(msg, ret=1):
+  """When debug is enabled, output is printed to the display.
+
+ returns nothing"""
+  type = ['WARNING', 'ERROR']
+  out = '%s: %s' % (type[ret], msg)
+  if (ret == 0):
+    print out
+  elif (ret == 1):
+    out += '\n'
+    sys.stderr.write(out)
+    cleanUp(ret)
+
+def cleanUp(ret):
+  """Do a clean and proper sys.exit()
+
+ executes sys.exit(int(ret))"""
+  sys.exit(ret)
+
+
+"""
+ Define Primary Functions
+"""
+def getSSHbinary():
+  """
+  Try to locate the ssh binary using which.
+  If 'None' we found nothing
+  If 'False' it is not executeable (shouldn't happen, but why not test?)
+
+  returns str(ssh_bin)
+	"""
+  ssh_bin = os.popen('which ssh').read().strip()
+  if (ssh_bin == ''):
+    ssh_bin = None
+  elif (os.access(ssh_bin, os.X_OK)):
+    ssh_bin = False
+  return ssh_bin
+
+def getHostsFromFile(get_from=None, known=False, verbose=True):
+  """
+ Read hosts from provided file.  If no file specified,
+ try to read ~/.ssh/known_hosts
+
+ returns list(hosts)
+ """
+  debugOut('getHostsFromFile(get_from=%s, known=%s, verbose=%s)' % (get_from,
+      known, verbose))
+  global host_files
+  if (get_from is not None):
+    if (os.access(get_from, os.R_OK)):
+      standardOut('Using user-defined host list from file: %s' % (
+        get_from), verbose)
+      host_files = [get_from]
+  else:    
+    if (known):
+      # First check to see if we can even get useful data out of the 
+      # known_hosts file
+      execute = "grep HashKnownHosts %s | awk '{print $2}'" % (ssh_config)
+      debugOut(execute, '\tExecuting')
+      is_hash = os.popen(execute).read().strip()
+      debugOut(is_hash, '\tIs known_hosts hashed')
+      if (is_hash == "yes"):
+        msg = "The known_hosts files are hashed.  Please "
+        msg += "specify host or alternate file to parse at cli"
+        errorOut(msg)
+      for f in host_files:
+        try:
+          os.path.exists(f)
+        except:
+          pass
+        else:
+          host_files.remove(f)
+  hosts = []
+  for x in host_files:
+    execute = 'cat %s | cut -f1 -d" " | sed -e "s/,.*//g" | uniq' % (x)
+    debugOut(execute, '\tExecuting')
+    si, so, se = os.popen3(execute)
+    for h in so.readlines():
+      h = h.strip()
+      debugOut(h, '\tFound Host')
+      hosts.append(h)
+  return list(hosts)
+
+def getIdentity(id_file=None, verbose=True):
+  """Read the identity file into a variable so that it is easier to push
+to the external host(s)
+
+ returns str(identity)"""
+  debugOut('getIdentity(id_file=%s, verbose=%s)' % (id_file, verbose))
+  global id_files
+  if (id_file is not None):
+    debugOut(id_file, '\tOver-riding id_files with')
+    over_ride = True
+    id_files = [os.path.abspath(os.path.expanduser(id_file))]
+    standardOut('\tReading identity from:\n\t\t%s' % (id_file), verbose)
+  for id_file in id_files:
+    debugOut(id_file, '\tTrying to grab identity from')
+    if (os.access(id_file, os.R_OK)):
+      identity = os.popen('head -1 %s' % (id_file)).read().strip()
+      msg = ''
+      break
+    else:
+      if (over_ride):
+        msg = 'Could not find/access specified file, %s' % (id_file)
+      else:
+        msg = 'Could not find/access default identity files'
+  if (msg is ''):
+    standardOut('\tFound identity:\n\t\t%s' % (id_file), verbose)
+    debugOut(identity, '\tUsing identity')
+  else:
+    errorOut(msg)
+  return str(identity)
+
+def toggleAgentForwarding(on, ssh_call, end_ssh_call, verbose=True):
+  """
+ This function 
+ """
+  command = ''
+  forward_option = 'ForwardAgent'
+  ssh_user_config_file = os.path.join(ssh_home_dir, 'config')
+  command +=  'grep -v \"%s\" %s > %s 2> /dev/null;' % (
+    forward_option, ssh_user_config_file, tmp_file)
+  command += 'mv -f %s %s 2>/dev/null; chmod 600 %s 2>/dev/null;' % (
+    tmp_file, ssh_user_config_file, ssh_user_config_file)
+  if (on):
+    command += 'echo \'%s yes\' >> %s 2> /dev/null; grep \"%s\" %s' % (
+      forward_option, ssh_user_config_file, forward_option, ssh_user_config_file)
+  remote_command=ssh_call+command+end_ssh_call
+  debugOut(remote_command, 'Executing')
+  ret = os.popen(remote_command).readlines()
+  if (ret):
+    if (on):
+      status = (True, 'enabled')
+    else:
+      status = (False, 'failed to disable')
+    for line in ret:
+      debugOut(line)
+  else:
+    if (on):
+      status = (False, 'failed to enable')
+    else:
+      status = (True, 'disabled')
+  return status
+  #standardOut('\t\tAgent Forwarding: %s' % (status), verbose)
+
+def pushToRemote(vars, options, hosts):
+  for host in hosts:
+    ssh_call = '%s -qq %s %s \'' % (vars.ssh_bin, options.userid, host)
+    end_ssh_call = '\''
+    standardOut('\tWorking on host: %s' % (host),
+                options.verbose)
+    if (options.forward):
+      toggleAgentForwarding(False, ssh_call, end_ssh_call, options.verbose)
+    command = ''
+    try:
+      old_identity
+    except:
+      use_identity = identity
+    else:
+      use_identity = old_identity
+    # Make ssh home dir and set permissions
+    command += 'mkdir %s &> /dev/null; chmod 700 %s;' % (
+      ssh_home_dir, ssh_home_dir)
+    # Remove identity from file and set permissions
+    command +=  'grep -v \"%s\" %s > %s 2> /dev/null;' % (
+      identity, auth_keys_file, tmp_file)
+    command += 'mv -f %s %s 2>/dev/null;' % (tmp_file, auth_keys_file)
+    command += 'chmod 600 %s 2> /dev/null;' % (auth_keys_file)
+    if (not options.remove or options.old_id_file):
+      use_identity = identity
+      # Then append the pub identity into the authorized_keys file, and grep it
+      command += 'echo \'%s\' >> %s 2> /dev/null; chmod 600 %s; grep \"%s\" %s' % (
+        use_identity, auth_keys_file, auth_keys_file, use_identity, auth_keys_file)
+    remote_command=ssh_call+command+end_ssh_call
+    debugOut(remote_command, 'Executing')
+    ret = os.popen(remote_command).readlines()
+    if (ret):
+      if (options.old_id_file):
+        status = 'changed'
+      elif (options.remove):
+        status = 'failed to remove'
+      else:
+        status = 'deployed'
+      for line in ret:
+        debugOut(line)
+    else:
+      if (options.remove):
+        status = 'removed'
+      else:
+        status = 'failed to deploy'
+    standardOut('\t\tPublic Identity Key: %s' % (status), options.verbose)
+
+    if (options.forward):
+      if (not options.remove or options.old_id_file):
+        toggleAgentForwarding(True, ssh_call, end_ssh_call, options.verbose)
